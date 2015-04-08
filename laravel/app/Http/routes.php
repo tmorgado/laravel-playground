7c61e4e05858735585cab9470843d0a8574b0cf8
Tiago Morgado <tiago.morgado@whitekube.com>
2015-04-08 19:51:02 +0100

    created pageControler

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -11,12 +11,3 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-Route::get('/contact', 'WelcomeController@contact');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
