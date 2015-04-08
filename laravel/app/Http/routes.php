55707c2452efbb7359c265d8dc774da9933fe3fc
Tiago Morgado <tiago.morgado@whitekube.com>
2015-04-08 20:23:24 +0100

    passing name variable to controler.

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -11,3 +11,5 @@
 |
 */
 
+
+Route::get('about', 'PagesController@about');
---
+++ laravel/resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+</head>
+<body>
+	<h1>About: {{ $first }} {{ $last }}</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus unde iure velit vitae vel? Vel ullam, nemo sed nulla! Hic incidunt consectetur sapiente ipsam recusandae quia saepe repellat doloremque alias!</p>
+</body>
+</html>
